3f7b85743dd0ec9d54d2844659b0febd9b0bf03e
Marquillier <nicolas.marquillier@gmail.com>
2021-06-15 11:53:00 +0200

    Chap 24 - Passer des méthodes entre les composants

--- src/App.js
+++ src/App.js
@@ -1,71 +1,59 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
-import Membre from './components/Membre'
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Membre from "./components/Membre";
+import Button from "./components/Button";
 
 const famille = {
   membre1: {
-    nom: 'Nicolas',
-    age: 35
+    nom: "Nicolas",
+    age: 35,
   },
   membre2: {
-    nom: 'Thomas',
-    age: 31
+    nom: "Thomas",
+    age: 31,
   },
   membre3: {
-    nom: 'Julien',
-    age: 39
+    nom: "Julien",
+    age: 39,
   },
   membre4: {
-    nom: 'Nesquik',
-    age: 4
+    nom: "Nesquik",
+    age: 4,
   },
-}
+};
 
 class App extends Component {
   state = {
-    famille
-  }
+    famille,
+  };
 
-  handleClick = () => {
-    const famille = { ...this.state.famille}
-      famille.membre1.age += 1
-    this.setState({famille})
-  }
+  handleClick = (num) => {
+    const famille = { ...this.state.famille };
+    famille.membre1.age += num;
+    this.setState({ famille });
+  };
 
   render() {
     // on peut tout importer en destructuring comme ci-dessous pour un code plus simple et plus lisible
-    const { titre,  } = this.props
-    const { famille } = this.state
+    const { titre } = this.props;
+    const { famille } = this.state;
     return (
       // Méthode d'écrire de code = JSX
       <Fragment>
         <div className="App">
-          <h1>{ titre }</h1>
-          <Membre 
-            nom = {famille.membre1.nom}
-            age = {famille.membre1.age}
-          />
-          <Membre 
-            nom = {famille.membre2.nom}
-            age = {famille.membre2.age}
-          />
-          <Membre 
-            nom = {famille.membre3.nom}
-            age = {famille.membre3.age}
-          />
-          <Membre 
-            nom = {famille.membre4.nom}
-            age = {famille.membre4.age}
-          >
-          {/* le contenu de la balise Membre est considéré comme un 'Children' 
+          <h1>{titre}</h1>
+          <Membre nom={famille.membre1.nom} age={famille.membre1.age} />
+          <Membre nom={famille.membre2.nom} age={famille.membre2.age} />
+          <Membre nom={famille.membre3.nom} age={famille.membre3.age} />
+          <Membre nom={famille.membre4.nom} age={famille.membre4.age}>
+            {/* le contenu de la balise Membre est considéré comme un 'Children' 
               c'est à dire un autre style de 'props' */}
-            Je suis un <strong><em>chat</em></strong>.
+            Je suis un{" "}
+            <strong>
+              <em>chat</em>
+            </strong>
           </Membre>
-          <button
-          onClick={this.handleClick}
-          >
-            viellir
-          </button>
+          <Button vieillir={() => this.handleClick(2)} />
         </div>
       </Fragment>
       // Autre façon d'écrire le code
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ vieillir }) => (
+  <button onClick={vieillir}>vieillir de 2 ans</button>
+)
+
+export default Button;
