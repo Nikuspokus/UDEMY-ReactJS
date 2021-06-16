3f203812940ba76552c8d295c8c3585127872058
Marquillier <nicolas.marquillier@gmail.com>
2021-06-16 11:23:29 +0200

    Chap 37 - Manipuler le State via un élément d'une liste

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import Membre from "./components/Membre";
-import Button from "./components/Button";
+import './components/Button.css'
 
 const famille = {
   membre1: {
     nom: "Julien",
     age: 39,
+    description: <p>je suis l'aîné.</p>,
   },
   membre2: {
     nom: "Nicolas",
     age: 35,
+    description: <p>je suis le cadet.</p>,
   },
   membre3: {
     nom: "Thomas",
     age: 31,
+    description: <p>Je suis le benjamin.</p>,
   },
   membre4: {
     nom: "Nesquik",
     age: 4,
+    description: (
+      <p>
+        Je suis un{" "}
+        <strong>
+          <em>chat</em>
+        </strong>
+        .
+      </p>
+    ),
+  },
+  membre5: {
+    nom: "toto",
+    age: 22,
+    description: (
+      <p>
+        Je suis un{" "}
+        <strong>
+          <em>chat</em>
+        </strong>
+        .
+      </p>
+    ),
   },
 };
 
 class App extends Component {
   state = {
     famille,
-    isShow: false
+    isShow: false,
   };
 
   handleClick = (num) => {
@@ -35,44 +60,56 @@ class App extends Component {
   };
 
   handleChange = event => {
-    const famille = { ...this.state.famille };
-    const nom = event.target.value
+    const famille = { ...this.state.famille }
+    const nom = event.target.value;
     // console.log(nom);
-    famille.membre2.nom = nom
+    famille.membre1.nom = nom;
+    this.setState({ famille });
+  };
+  
+  cacherNom = id => {
+    const famille = { ...this.state.famille }
+    famille[id].nom = 'X'
     this.setState({ famille });
   };
 
   handleShowDescription = () => {
-    const isShow = !this.state.isShow
-    this.setState({ isShow })
-  }
+    const isShow = !this.state.isShow;
+    this.setState({ isShow });
+  };
 
   render() {
     // on peut tout importer en destructuring comme ci-dessous pour un code plus simple et plus lisible
     const { titre } = this.props;
-    const { famille, isShow } = this.state;
+    const { famille, } = this.state;
+
+    const liste = Object.keys(famille)
+    .map(membre => (
+      <Membre
+      cacherNom={() => this.cacherNom(membre)}
+      age={famille[membre].age}
+      nom={famille[membre].nom}
+      />
+    ) )
+
     return (
       // Méthode d'écrire de code = JSX
       <Fragment>
         <div className="App">
           <h1>{titre}</h1>
-          <input value={famille.membre1.nom} onChange={this.handleChange} type="text" />
-          <Membre nom={famille.membre1.nom} age={famille.membre1.age} />
-          <Membre nom={famille.membre2.nom} age={famille.membre2.age} />
-          <Membre nom={famille.membre3.nom} age={famille.membre3.age} />
-          <Membre nom={famille.membre4.nom} age={famille.membre4.age}>
-            {/* le contenu de la balise Membre est considéré comme un 'Children' 
-              c'est à dire un autre style de 'props' */}
-            {
-              isShow ? <p>Je suis un <strong><em>chat</em></strong>.</p> : null
-            }
+          <input
+            value={famille.membre1.nom}
+            onChange={this.handleChange}
+            type="text"
+          />
+          {liste}
+          {/* <Membre nom={famille.membre4.nom} age={famille.membre4.age}> 
+           {isShow ? description : null}
             <button onClick={this.handleShowDescription}>
-            {
-              isShow ? 'Cacher' : 'Montrer'
-            }
+              {isShow ? "Cacher" : "Montrer"}
             </button>
-          </Membre>
-          <Button vieillir={() => this.handleClick(2)} />
+          </Membre> */}
+          {/* <Button vieillir={() => this.handleClick(2)} /> */}
         </div>
       </Fragment>
       // Autre façon d'écrire le code
--- src/components/Membre.js
+++ src/components/Membre.js
@@ -1,17 +1,21 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from "react";
 
-const Membre = ({ nom, age, children }) => {
 
+const Membre = ({ nom, age, children, cacherNom }) => {
   return (
     <Fragment>
-    <h2 style={{ 
-        backgroundColor: age >= 35 ? 'purple' : 'yellow', 
-        color:  age >= 35 ? 'white' : 'black' }}>
-        { nom.toUpperCase() } : { age }
+      <h2
+        style={{
+          backgroundColor: age >= 35 ? "purple" : "yellow",
+          color: age >= 35 ? "white" : "black",
+        }}
+      >
+        {nom.toUpperCase()} : {age}
       </h2>
-    { children ? <p>{children}</p> : null}
+      <button onClick={cacherNom}>X</button>
+      {children ? <p>{children}</p> : null}
     </Fragment>
-  )
-}
+  );
+};
 
-export default Membre
+export default Membre;
